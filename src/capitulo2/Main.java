e142092412fa99740d424f56567c20f5ba34f350
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-16 15:20:22 -0300

    Exercícios: modificando o Hello World

---
+++ src/capitulo2/Main.java
@@ -0,0 +1,9 @@
+package capitulo2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //exercício: modificando o Hello World
+        System.out.println("Mensagem \n diferente");
+    }
+}
