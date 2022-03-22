b6192e8c728c82104088ddeb2d9889b7801dab5e
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2022-03-22 11:41:44 -0300

    Exercício 18.5

--- src/capitulo18/Teste.java
+++ src/capitulo18/Teste.java
@@ -6,9 +6,15 @@ public class Teste {
         p1.setId(1);
         Thread t1 = new Thread(p1);
         t1.start();
-        Programa p2 = new Programa();
-        p2.setId(2);
-        Thread t2 = new Thread(p2);
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    System.out.println("Programa com classe anonima" + "	valor: " + i);
+                }
+            }
+        };
+        Thread t2 = new Thread(runnable);
         t2.start();
         //O resultado pode mudar a cada vez que o código é executado por conta da thread
     }
